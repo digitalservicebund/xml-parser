3555dce81c1bd48c6981e4f3f03ab13c3f66d196
Haytham Mones <haythameyd@gmail.com>
2023-10-27 08:35:45 +0200

    Removed github packages pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,16 +112,6 @@ tasks {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/digitalservicebund/xml-parser")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
